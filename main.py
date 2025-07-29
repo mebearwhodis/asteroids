9972df7ac35f36c0f15a91886cc830566c69fc7b
mebearwhodis <mebearwhodis@gmail.com>
2025-07-29 17:12:10 +0200

    Asteroid class

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
-    
+    Asteroid.containers = (asteroids, updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
