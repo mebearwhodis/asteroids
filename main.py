ca8aa981591264368518eaf34a7c11659c798bec
mebearwhodis <mebearwhodis@gmail.com>
2025-07-29 16:33:19 +0200

    limit fps

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        #Limit framerate to 60 fps
+        dt = clock.tick(60) /1000
 
 
 if __name__ == "__main__":
