08e0f2cef43f761fc29920d4fce79bc752329e74
mebearwhodis <mebearwhodis@gmail.com>
2025-07-29 16:54:57 +0200

    added player

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import *
+
 
 def main():
     print("Starting Asteroids!")
@@ -11,11 +13,17 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         #Limit framerate to 60 fps
